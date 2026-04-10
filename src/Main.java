36f6feeec4234dae5a03a8513ea01da04d142e05
Jayden <jayden.chanzz06@gmail.com>
2026-04-10 18:10:46 -0400

    Added file loading Completed generalized view for students, staff, and faculty.

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,50 @@
 import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.Buffer;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Main {
-    public Main() {
-        HashMap<String, Runnable> menu_options = new HashMap<>();
-        menu_options.put("1", View::new);
+    private final BufferedReader userInput;
+    static Boolean quit = false;
+
+    Main() {
+        String mainMenu = loadMenuText("mainMenu.txt");
+        String errorMessage = loadMenuText("menuError.txt");
+        ArrayList<ArrayList> students = loadRecords("studentRecords.txt");
+        ArrayList<ArrayList> faculty = loadRecords("facultyRecords.txt");
+        ArrayList<ArrayList> staff = loadRecords("staffRecords.txt");
+
+        userInput = new BufferedReader(new InputStreamReader(System.in));
+
+        HashMap<String, Runnable> menuOptions = new HashMap<>();
+        menuOptions.put("1", () -> new View(students, userInput));
+        menuOptions.put("2", () -> new View(faculty, userInput));
+        menuOptions.put("3", () -> new View(staff, userInput));
+        menuOptions.put("Q", () -> quit());
+
         Boolean error = false;
 
-        try (BufferedReader user_input = new BufferedReader(new InputStreamReader(System.in))) {
-            String main_menu = loadMenuText("main_menu.txt");
-            String error_message = loadMenuText("menu_error.txt");
-
+        try {
+            menuLoop:
             while (true) {
-                System.out.println(main_menu);
+                if (quit) {
+                    System.out.println("Program shutdown \nHasta la vista!");
+                    break;
+                }
+                System.out.println(mainMenu);
 
                 if (error) {
-                    System.out.println(error_message);
+                    System.out.println(errorMessage);
                     error = false;
                 }
 
-                String input = user_input.readLine().toUpperCase();
+                String input = userInput.readLine().toUpperCase();
 
-                if (menu_options.containsKey(input)) {
-                    menu_options.get(input).run();
-                } else if (input.equals("Q")) {
-                    System.out.println("Program shutdown \nHasta la vista!");
-                    break;
+                if (menuOptions.containsKey(input)) {
+                    menuOptions.get(input).run();
                 } else {
                     error = true;
                 }
@@ -42,10 +61,10 @@ class Main {
         String line;
 
         try {
-            BufferedReader file_reader = new BufferedReader(new FileReader(fileName));
+            BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
 
-            while ((line = file_reader.readLine()) != null) {
-                result.concat(line + "\n");
+            while ((line = fileReader.readLine()) != null) {
+                result += line + "\n";
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -53,6 +72,30 @@ class Main {
 
         return result;
     }
+
+
+    static ArrayList loadRecords(String fileName) {
+        ArrayList<ArrayList> result = new ArrayList<>();
+        String line;
+
+        try {
+            BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+
+            while ((line = fileReader.readLine()) != null) {
+                result.add(new ArrayList<>(Arrays.asList(line.split("\\s*,\\s*"))));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    static void quit() {
+        quit = true;
+    }
+
 
     static void main(String[] args) {
         new Main();
--- src/View.java
+++ src/View.java
@@ -1,5 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.*;
+
 public class View {
-    View() {
-        System.out.println("View menu");
+    /* Use this class to load the list of students, faculty, or staff */
+
+    View(ArrayList records, BufferedReader userInput) {
+        String errorMessage = Main.loadMenuText("menuError.txt");
+
+        try {
+            while (true) {
+                listRecords(records);
+
+                String input = userInput.readLine().toUpperCase();
+
+                if (input.equals("Q")) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    static void listRecords(ArrayList<ArrayList> records) {
+        int recordNo = 1;
+        Boolean column1 = true;
+
+        System.out.println("--------------------");
+
+        for (ArrayList<String> record : records) {
+            String row = Integer.toString(recordNo) + " - " + record.get(0);
+
+            if (column1) {
+                System.out.printf("%-50s", row);
+
+            } else {
+                System.out.printf("%-50s", row);
+                System.out.println();
+            }
+
+            recordNo += 1;
+
+            column1 = !column1;
+        }
+
+        if (!column1) {
+            System.out.println();
+        }
+
+        System.out.println("\nA - Add new student\nQ - Return to main menu\n");
     }
 }
